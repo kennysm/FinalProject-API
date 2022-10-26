bb23fa4ec2e1398a2e496e67d84e0840ab8fe2ac
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-10-26 14:32:41 -0300

    connecting database

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost/javascriptNote", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log(err));
